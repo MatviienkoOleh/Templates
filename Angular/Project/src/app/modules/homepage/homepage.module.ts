49e62e878df72060fa9051a99c537e66d962308b
MatviienkoOleh <matviienkooleh@gmail.com>
2021-02-22 22:47:21 +0200

    commit

---
+++ Angular/Project/src/app/modules/homepage/homepage.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomepageComponent } from './component/homepage/homepage.component';
+
+
+
+@NgModule({
+  declarations: [HomepageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomepageModule { }
---
+++ Angular/Project/src/app/modules/index.ts
@@ -0,0 +1,8 @@
+import { FormModule } from "./form/form.module";
+import { HomepageModule } from "./homepage/homepage.module";
+
+
+export const modules = [
+    HomepageModule,
+    FormModule
+];
